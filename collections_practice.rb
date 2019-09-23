aea7f502e8255884f5f09780ec451b639e57b31d
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-09-23 14:21:40 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -35,8 +35,11 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect do |word, index| + "s"
-  
+  array.collect.with_index do |word, index|
+    if index != 1 
+      word + "s"
+    end
+  end
 end
 
 
